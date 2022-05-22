2895699fe850b800d39bc248787d1820f48acaff
Jabi <michal.jabi@gmail.com>
2022-05-23 00:55:51 +0200

    refactor(1st-attempt): make it right - refactor of the 1-st solution

--- src/1st-attempt.js
+++ src/1st-attempt.js
@@ -1,50 +1,36 @@
-// Bierzemy referencje do wszystkich obsługiwanych elementów DOM:
 const $list = document.querySelector('ul');
 const $fruits = document.querySelectorAll('li');
 const [ $btnSortByName, $btnSortByPrice ] = document.querySelectorAll('button');
 
-// Ustalamy, jaka jest kolejność sortowania aktualnie:
-let direction = 'ASC';
+const DIRECTIONS = { ASC: 'ASC', DESC: 'DESC' };
+
+const getBaseAccordingToDirection = (a, b) => (direction === DIRECTIONS.ASC) ? {base1: a, base2: b} : {base1: b, base2: a};
+const getFruitName = el => el.innerText.toLowerCase().trim();
+const getFruitPrice = el => {
+	const [textPrice] = el.innerText.match(/\d\.?\d*/g) || ['0'];
+	return Number(textPrice);
+}
+const domRefs = Array.from($fruits);
+
+let direction = DIRECTIONS.ASC;
 
 $btnSortByName.addEventListener('click', () => {
-	 const domRefs = Array.from($fruits);
-	 domRefs.sort((e1, e2) => {
-		 let base1, base2;
-		 const getText = el => el.innerText.toLowerCase().trim();
-		 if(direction === 'ASC') {
-			 base1 = e1;
-			 base2 = e2;
-		 } else {
-			 base1 = e2;
-			 base2 = e1;
-		 }
-		 return getText(base1).localeCompare(getText(base2))
-	 } );
-	 direction = direction === 'ASC' ? 'DESC' : 'ASC';
-	 // Jeszcze raz "Wpinam" posortowane elementy do DOM
-	 // korzystając z własność, że na drzewie DOM nie może występować 2 razy ta sama referencja:
-	 $list.append(...domRefs);
+	domRefs.sort((e1, e2) => {
+		const { base1, base2 } = getBaseAccordingToDirection(e1, e2)
+		return getFruitName(base1).localeCompare(getFruitName(base2))
+	} );
+	updateDOM();
 })
 
-// Kod obsługujący 2 button — to w większości powtórzenia...
-// Jednak nie szkodzi - MAKE IT WORK!
 $btnSortByPrice.addEventListener('click', () => {
-	const domRefs = Array.from($fruits);
 	domRefs.sort((e1, e2) => {
-		let base1, base2;
-		const getPrice = el => {
-			const [textPrice] = el.innerText.match(/\d\.?\d*/g) || ['0'];
-			return Number(textPrice);
-		}
-		if(direction === 'ASC') {
-			base1 = e1;
-			base2 = e2;
-		} else {
-			base1 = e2;
-			base2 = e1;
-		}
-		return getPrice(base1) - getPrice(base2)
-	} );
-	direction = direction === 'ASC' ? 'DESC' : 'ASC';
-	$list.append(...domRefs);
+		const { base1, base2 } = getBaseAccordingToDirection(e1, e2)
+		return getFruitPrice(base1) - getFruitPrice(base2)
+	});
+	updateDOM();
 })
+
+function updateDOM() {
+	direction = (direction === DIRECTIONS.ASC) ? DIRECTIONS.DESC : DIRECTIONS.ASC;
+	$list.append(...domRefs);
+}
